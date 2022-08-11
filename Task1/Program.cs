5f8a741a81fd60ea98972bb80424d7dcc0e9c4dc
Elena Sinyagina <lenaakimova08@rambler.ru>
2022-08-11 12:04:30 +0400

    Task 1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+Console.WriteLine("Введите число и нажмите Enter:");
+
+string? numberString = Console.ReadLine();
+
+int number = int.Parse(numberString);
+
+int number1 = number / 10;
+int result = number1 % 10;
+if(result < 0)
+{
+    result = result * (-1);
+    Console.WriteLine("Вторая цифра числа: " + result);
+}
+else 
+{
+    Console.WriteLine("Вторая цифра числа: " + result);
+}
